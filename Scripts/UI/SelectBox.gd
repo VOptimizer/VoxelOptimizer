309d288532f035473fab04d38b27602576f518c1
tostc <46672880+tostc@users.noreply.github.com>
2024-02-11 17:26:50 +0100

    - New mesher and bugfixes

--- Scripts/UI/SelectBox.gd
+++ Scripts/UI/SelectBox.gd
@@ -7,6 +7,7 @@ func _ready():
 	_SelectBox.add_item("Greedy")
 	_SelectBox.add_item("Marching cubes")
 	_SelectBox.add_item("Greedy Chunked")
+	_SelectBox.add_item("Greedy Textured (Beta)")
 	#_SelectBox.add_item("Flood")
 	
 	_SelectBox.select(0)
